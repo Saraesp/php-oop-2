1568ada20c7a69218b9e902162376cbfe0c9834c
Saraesp <saraesposito145@gmail.com>
2023-02-07 10:18:08 +0100

    creazione database

---
+++ database/database.php
@@ -0,0 +1,7 @@
+<?php
+    include __DIR__ . './Model/Prodotto'
+    include __DIR__ . './Model/Categoria'
+    include __DIR__ . './Model/Gioco'
+    include __DIR__ . './Model/Cibo'
+    include __DIR__ . './Model/Acessori'
+?>
