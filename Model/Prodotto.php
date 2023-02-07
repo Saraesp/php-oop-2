1d7e5ea594df699179af44e1e64036172be0a56b
Saraesp <saraesposito145@gmail.com>
2023-02-07 09:52:05 +0100

    impostazione file categoria e prodotto

---
+++ Model/Prodotto.php
@@ -0,0 +1,16 @@
+<?php
+
+    class Prodotto{
+        public $product_name;
+        public $price;
+        public $image;
+        public $category;
+
+        public function __construct($product_name, $price, $image, Categoria $category){
+            $this->product_name =$product_name;
+            $this->price =$price;
+            $this->image =$image;
+            $this->category =$category;
+        }
+    }
+?>
